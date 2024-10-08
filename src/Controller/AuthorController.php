31bc71a7e97bda96cce61d1d3472bf7a125aeb8f
Nour Houda <nourmelki05@gmail.com>
2024-10-08 09:53:01 +0100

    commit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\AuthorType;
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+    #[Route('/showAuthor/{name}', name: 'app_showAuthor')]
+
+    public function showAuthor ($name)
+    {
+        return $this->render('author/show.html.twig',['n'=>$name]);
+
+    }
+    #[Route('/showlist', name: 'app_showlist')]
+
+    public function list()
+    {$authors = array(
+        array('id' => 1, 'picture' => '/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' =>
+            'victor.hugo@gmail.com ', 'nb_books' => 100),
+        array('id' => 2, 'picture' => '/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>
+            ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+        array('id' => 3, 'picture' => '/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' =>
+            'taha.hussein@gmail.com', 'nb_books' => 300),
+    );
+        return $this->render("author/list.html.twig",['authors'=>$authors]);
+    }
+    #[Route('/auhtorDetails/{id}', name: 'app_authorDetails')]
+
+    public function auhtorDetails»($id)
+    {
+        $author = [
+            'id' => $id,
+            'picture' => '~images',
+            'username' => 'Author',
+            'email' => 'author.email',
+            'nb_books' => 10,
+        ];
+
+        return $this->render("author/showAuthor.html.twig",['author'=>$author]);
+    
+    }
+    
+}
